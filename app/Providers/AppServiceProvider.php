4ac2db6ffb8710620eab6f538230eac375572711
Francis <reisenseo@gmail.com>
2026-03-16 18:15:50 +0300

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (config('session.driver') === 'database') {
+            config(['session.driver' => 'file']);
+        }
+
+        if (config('cache.default') === 'database') {
+            config(['cache.default' => 'file']);
+        }
     }
 
     /**
